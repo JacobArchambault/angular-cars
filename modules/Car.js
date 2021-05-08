e523a14bd32d44ae080a3fb1b16dce11b70cdfca
Jacob Archambault <jacobarchambault@gmail.com>
2021-05-08 12:39:42 -0400

    Clean up car.js

--- modules/Car.js
+++ modules/Car.js
@@ -2,16 +2,14 @@ var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/cars4sale');  // db name = cars4sale
 
-var carSchema = new mongoose.Schema({
-	cid: {type: Number, required: true, unique: true},
-	year: {type: Number, required: true},
-	make: {type: String, required: true},
-	model: {type: String, required: true},
-	miles: {type: Number, required: true},
-	price: {type: Number, required: true},
-	dealer_id: {type: String, required: true},
-});
-
-module.exports = mongoose.model('Car', carSchema);
+module.exports = mongoose.model('Car', new mongoose.Schema({
+	cid: { type: Number, required: true, unique: true },
+	year: { type: Number, required: true },
+	make: { type: String, required: true },
+	model: { type: String, required: true },
+	miles: { type: Number, required: true },
+	price: { type: Number, required: true },
+	dealer_id: { type: String, required: true },
+}));
 
 
